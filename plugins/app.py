085ba670a3aae1232bdb7cfac9113cb5b16c91ee
Sion1073 <Sion1073@gmail.com>
2020-03-03 11:35:09 +0900

    日本時刻に対応

--- plugins/app.py
+++ plugins/app.py
@@ -1,5 +1,5 @@
 import random
-import datetime
+from datetime import datetime, timedelta, timezone
 
 from slackbot.bot import respond_to
 
@@ -10,6 +10,8 @@ def omikuji(message):
 
 @respond_to('出勤')
 def work_time(message):
-    start_now = datetime.datetime.now()
-    end_time = start_now + datetime.timedelta(hours=8, minutes=45)
+
+    JST = timezone(timedelta(hours=+9), 'JST')
+    start_now = datetime.now(JST)
+    end_time = start_now + timedelta(hours=8, minutes=45)
     message.reply(end_time.strftime("%H:%M"))
