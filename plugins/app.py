e9cd79b1ae000c29559e50c0ef52f9369f1566bc
Sion1073 <Sion1073@gmail.com>
2020-03-03 10:42:23 +0900

    退勤時間連絡botを追加

--- plugins/app.py
+++ plugins/app.py
@@ -1,7 +1,15 @@
-from slackbot.bot import respond_to
 import random
+import datetime
 
+from slackbot.bot import respond_to
 
 @respond_to('おみくじ')
 def omikuji(message):
     message.reply(random.choice(['大吉', '吉', '中吉', '小吉', '末吉', '凶', '大凶']))
+
+
+@respond_to('出勤')
+def work_time(message):
+    start_now = datetime.datetime.now()
+    end_time = start_now + datetime.timedelta(hours=8, minutes=45)
+    message.reply(end_time.strftime("%H:%M"))
