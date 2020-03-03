9cb88152e1bec31d44d4309fa3112dd7c0c72967
Sion1073 <Sion1073@gmail.com>
2020-03-03 13:04:03 +0900

    正規表現で出勤時間指定に変更

--- plugins/app.py
+++ plugins/app.py
@@ -1,3 +1,4 @@
+import re
 import random
 from datetime import datetime, timedelta, timezone
 
@@ -8,10 +9,24 @@ def omikuji(message):
     message.reply(random.choice(['大吉', '吉', '中吉', '小吉', '末吉', '凶', '大凶']))
 
 
-@respond_to('出勤')
+@listen_to(r'^出勤\s[0-9]+:[0-9]+$')
 def work_time(message):
 
     JST = timezone(timedelta(hours=+9), 'JST')
-    start_now = datetime.now(JST)
+    current = datetime.now(JST)
+
+    text = message.body['text']
+    result = re.match(".*\s([0-9]+):([0-9]+)", content)
+
+    hour = result.group(1)
+    minute = result.group(2)
+
+    start_now = datetime.datetime(
+        year=current.year,
+        month=current.month,
+        day=current.day,
+        hour=hour,
+        minute=minute,
+        second=0)
     end_time = start_now + timedelta(hours=8, minutes=45)
     message.reply(end_time.strftime("%H:%M"))
